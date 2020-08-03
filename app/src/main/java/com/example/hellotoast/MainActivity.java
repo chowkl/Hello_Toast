fe7d9de1f623240e7d29b6ecb39f721f4ad599be
chowkl <chowkl-wm19@studnt.tarc.edu.my>
2020-08-03 16:35:20 +0800

    commit by chowkl

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.hellotoast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -10,12 +11,23 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private int mCount = 0;
     private TextView mShowCount;
+    private  TextView mMessageCount;
+    public static final String EXTRA_MESSAGE =
+            "com.example.android.hellotoast.extra.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        mMessageCount = (TextView) findViewById(R.id.show_count);
+    }
+    public void nextActivity(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        String message = mMessageCount.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
     }
 
     public void showToast(View view) {
